3f852cd873eab68131850bbc10c6551301d1fb82
Finny_ST <58487697+FinnyChSt@users.noreply.github.com>
2023-03-06 21:55:24 +0100

    add picture button (#7)

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import "./App.css";
 import HeartLogoFilled from "./assets/heart-svg-red.svg";
 import HeartLogoOutline from "./assets/heart-outlined-svg.svg";
 import avi from "./IMG_20221109_211721_174.jpg";
+import avi2 from "../src/assets/IMG_20230112_230338_050.jpg";
 import {
   Avatar,
+  AvatarWrapper,
   Background,
+  ButtonIcon,
+  ButtonWrapperLeft,
+  ButtonWrapperRight,
   CountdownDays,
   CountdownHours,
   HeartListContainer,
@@ -21,7 +26,7 @@ function App() {
   const startDate = new Date(2023, 1, 24, 21, 10).getTime();
 
   const [timeLeft, setTimeLeft] = useState(arrival - Date.now());
-
+  const [shownAvatar, setShownAvatar] = useState(avi);
   const daysBetween = (arrival - startDate) / (1000 * 60 * 60 * 24);
   const staticDaysLeft = (arrival - Date.now()) / (1000 * 60 * 60 * 24);
   const daysPass = daysBetween - staticDaysLeft;
@@ -51,13 +56,48 @@ function App() {
   const Hearts = () => {
     let HeartList = [];
     for (let heart = 0; heart < daysPass; heart++) {
-      HeartList.push(<HeartSvg src={HeartLogoFilled} key={heart} />);
+      HeartList.push(
+        <HeartSvg src={HeartLogoFilled} key={`${heart}-filled`} />
+      );
     }
     for (let heart = 0; heart < staticDaysLeft - 1; heart++) {
-      HeartList.push(<HeartSvg src={HeartLogoOutline} key={heart} />);
+      HeartList.push(
+        <HeartSvg src={HeartLogoOutline} key={`${heart}-outline`} />
+      );
     }
     return <>{HeartList}</>;
   };
+  const pictureList = [avi, avi2];
+  const changeAvatarLeft = () => {
+    const index = pictureList.indexOf(shownAvatar);
+    if (index === 0) {
+      setShownAvatar(pictureList[pictureList.length - 1]);
+    } else {
+      setShownAvatar(pictureList[index - 1]);
+    }
+  };
+  const changeAvatarRight = () => {
+    const index = pictureList.indexOf(shownAvatar);
+    if (index === pictureList.length - 1) {
+      setShownAvatar(pictureList[0]);
+    } else {
+      setShownAvatar(pictureList[index + 1]);
+    }
+  };
+  const IconButton = ({
+    icon,
+    onClick,
+  }: {
+    icon: any;
+    onClick: () => void;
+  }) => {
+    return (
+      <ButtonIcon onClick={onClick}>
+        <HeartSvg src={icon} />
+      </ButtonIcon>
+    );
+  };
+
   return (
     <Background>
       <PageWrapper>
@@ -65,7 +105,23 @@ function App() {
         <CountdownDays>{`Days until I see my Love again:${daysLeft}`}</CountdownDays>
         <CountdownHours>{`Hours until I can finally see my Love again ${hoursLeft}`}</CountdownHours>
         <Subtitle>
-          <Avatar src={avi}></Avatar>
+          <AvatarWrapper>
+            <ButtonWrapperLeft>
+              <IconButton
+                icon={HeartLogoFilled}
+                onClick={changeAvatarLeft}
+                key={"clickLeft"}
+              />
+            </ButtonWrapperLeft>
+            <Avatar src={shownAvatar}></Avatar>
+            <ButtonWrapperRight>
+              <IconButton
+                icon={HeartLogoFilled}
+                onClick={changeAvatarRight}
+                key={"clickRight"}
+              />
+            </ButtonWrapperRight>
+          </AvatarWrapper>
         </Subtitle>
         <LiveCountDown>{`Countdown for Happiness : ${CountDown}`}</LiveCountDown>
         <HeartListContainer>
